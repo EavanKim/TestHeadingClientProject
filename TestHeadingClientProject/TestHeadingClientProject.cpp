ea4b74d166c2ed55f579ca37c2073c8bd3ad359c
kjt21@kakao.com <kjtkjt21@gmail.com>
2023-06-28 06:02:30 +0900

    갓뎀

--- TestHeadingClientProject/TestHeadingClientProject.cpp
+++ TestHeadingClientProject/TestHeadingClientProject.cpp
@@ -1,38 +1,7 @@
 ﻿// TestHeadingProject.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
-#include <iostream>
-
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <ws2ipdef.h>
-
-#pragma comment(lib, "ws2_32.lib")
-#include <Windows.h>
-
-#pragma pack(push, 1)
-struct Header
-{
-	uint64_t type = 0;
-	uint64_t length = 0;
-};
-
-template<uint64_t _type, uint64_t _buffersize>
-struct SendStruct : public Header
-{
-	char buffer[ _buffersize ];
-
-	SendStruct()
-	{
-		type = _type;
-		length = sizeof( Header ) + _buffersize;
-	}
-};
-
-typedef SendStruct<1, 0> SessionKey;
-typedef SendStruct<2, 0> Shutdown;
-typedef SendStruct<100, 43> TestBuffer;
-#pragma pack(pop)
+#include "psudoPCH.h"
 
 uint64_t m_resultsize = 0;
 
@@ -78,35 +47,89 @@ int TryConnect(SOCKET& _sock, addrinfo* _info)
 	while( S_OK != result );
 }
 
+void DoRawCode()
+{
+	//uint64_t m_packetMulti = 1;
+	//uint64_t m_bufferSize = 0;
+
+	//uint64_t m_currentpacketSize = 0;
+	//uint64_t m_totalRecv = 0;
+
+	//WSADATA m_data = {};
+	//addrinfo* m_info = nullptr;
+	//SOCKET m_socket = INVALID_SOCKET;
+	//TestBuffer testbuffer;
+
+	//uint64_t testdata = 0;
+	//uint64_t testdata1 = 43;
+
+	//memcpy( &testbuffer.buffer, "!!! this is test Text this is test Text !!", 43 );
+
+	//char* sendBuffer = ( char* )malloc( sizeof( testbuffer ) * m_packetMulti );
+
+	//for( unsigned long count = 0; m_packetMulti > count; ++count )
+	//	memcpy( sendBuffer + ( count * sizeof( testbuffer ) ), &testbuffer, sizeof( testbuffer ) );
+
+	//printf("Created Packet Info \n");
+	//printf("TestBuffer \n");
+	//printf("type : %lld \n", testbuffer.type);
+	//printf("length : %lld \n", testbuffer.length);
+	//printf("Buffer : %s \n", testbuffer.buffer);
+
+	//addrinfo createData = {};
+
+	//SecureZeroMemory( ( PVOID )&createData, sizeof( addrinfo ) );
+
+	//createData.ai_family = AF_UNSPEC;
+	//createData.ai_socktype = SOCK_STREAM;
+	//createData.ai_protocol = IPPROTO_TCP;
+
+	//result = getaddrinfo( "127.0.0.1", "50000", &createData, &m_info );
+	//if( S_OK != result )
+	//{
+	//	// 초기화 실패
+	//	int sockerror = WSAGetLastError();
+	//	int winerror = GetLastError();
+	//	// exception 객체 생성되면 throw하면서 에러 정보 송신
+	//	return 1;
+	//}
+
+	//if( 0 != TryConnect( m_socket, m_info ) )
+	//	return 1;
+
+	//unsigned long long Count = 0;
+	//DWORD receiveSize = 0;
+	//while( 1 )
+	//{
+	//	for( uint64_t count = 0; 100 > count; ++count )
+	//	{
+	//		receiveSize = send( m_socket, sendBuffer, sizeof( testbuffer ) * m_packetMulti, 0 );
+	//		if( -1 == receiveSize )
+	//		{
+	//			int sockerror = WSAGetLastError();
+	//			int winerror = GetLastError();
+	//			// 그냥 끝냅니다.
+
+	//			closesocket( m_socket );
+	//			m_socket = INVALID_SOCKET;
+
+	//			if( 0 != TryConnect( m_socket, m_info ) )
+	//				return 1;
+	//		}
+	//	}
+
+	//	Sleep(1000);
+	//	++Count;
+	//}
+
+	//Shutdown endpacket;
+	//receiveSize = send( m_socket, (char*)&endpacket, sizeof(Shutdown), 0);
+	//closesocket(m_socket);
+}
+
 int main()
 {
-	uint64_t m_packetMulti = 1;
-	uint64_t m_bufferSize = 0;
-
-	uint64_t m_currentpacketSize = 0;
-	uint64_t m_totalRecv = 0;
-
 	WSADATA m_data = {};
-	addrinfo* m_info = nullptr;
-	SOCKET m_socket = INVALID_SOCKET;
-	TestBuffer testbuffer;
-
-	uint64_t testdata = 0;
-	uint64_t testdata1 = 43;
-
-	memcpy( &testbuffer.buffer, "!!! this is test Text this is test Text !!", 43 );
-
-	char* sendBuffer = ( char* )malloc( sizeof( testbuffer ) * m_packetMulti );
-
-	for( unsigned long count = 0; m_packetMulti > count; ++count )
-		memcpy( sendBuffer + ( count * sizeof( testbuffer ) ), &testbuffer, sizeof( testbuffer ) );
-
-	printf("Created Packet Info \n");
-	printf("TestBuffer \n");
-	printf("type : %lld \n", testbuffer.type);
-	printf("length : %lld \n", testbuffer.length);
-	printf("Buffer : %s \n", testbuffer.buffer);
-
 	int result = WSAStartup( MAKEWORD( 2, 2 ), &m_data );
 	if( S_OK != result )
 	{
@@ -117,55 +140,53 @@ int main()
 		return 1;
 	}
 
-	addrinfo createData = {};
-
-	SecureZeroMemory( ( PVOID )&createData, sizeof( addrinfo ) );
-
-	createData.ai_family = AF_UNSPEC;
-	createData.ai_socktype = SOCK_STREAM;
-	createData.ai_protocol = IPPROTO_TCP;
-
-	result = getaddrinfo( "127.0.0.1", "50000", &createData, &m_info );
-	if( S_OK != result )
-	{
-		// 초기화 실패
-		int sockerror = WSAGetLastError();
-		int winerror = GetLastError();
-		// exception 객체 생성되면 throw하면서 에러 정보 송신
-		return 1;
-	}
-
-	if( 0 != TryConnect( m_socket, m_info ) )
-		return 1;
-
-	unsigned long long Count = 0;
-	DWORD receiveSize = 0;
-	while( 1 )
-	{
-		for( uint64_t count = 0; 100 > count; ++count )
-		{
-			receiveSize = send( m_socket, sendBuffer, sizeof( testbuffer ) * m_packetMulti, 0 );
-			if( -1 == receiveSize )
-			{
-				int sockerror = WSAGetLastError();
-				int winerror = GetLastError();
-				// 그냥 끝냅니다.
-
-				closesocket( m_socket );
-				m_socket = INVALID_SOCKET;
-
-				if( 0 != TryConnect( m_socket, m_info ) )
-					return 1;
-			}
-		}
-
-		Sleep(1);
-		++Count;
-	}
+	//================================================================================================================================================================
+	// DoRawCode();
 	//================================================================================================================================================================
 
-	Shutdown endpacket;
-	receiveSize = send( m_socket, (char*)&endpacket, sizeof(Shutdown), 0);
-	closesocket(m_socket);
+	CSocket proxySocket("127.0.0.1", "50000");
+	CSocket broadCastSocket("127.0.0.1", "51000");
+
+	proxySocket.C_Connect();
+	broadCastSocket.C_Connect();
+
+	proxySocket.CreateSessionKey();
+	broadCastSocket.CreateSessionKey();
+
+	std::string buffer;
+	bool ProgramAlive = true;
+	while( ProgramAlive )
+	{
+		ChatBuffer sendbuffer;
+		// 단순한 구조인 지금 상황에서 매번 데이터를 갱신받으려면 빈 데이터라도 보내야합니다.
+		// 업데이트 요청을 기본으로 발송합니다.
+		memcpy( sendbuffer.buffer, "Request Update", 15 );
+		std::cin >> buffer;
+
+		uint64_t length = buffer.length();
+		if(1 == length)
+		{
+			if( '0' == buffer[ 0 ] )
+			{
+				// 여기서 바로 cleanup시키고 리턴해도 되지만 그냥 빠져나가기 처리로 합니다.
+				ProgramAlive = false;
+				continue;
+			}
+		}
+		
+		if( 0 != length )
+		{
+			ZeroMemory( &sendbuffer.buffer, sendbuffer.length - sizeof( Header ) );
+			// 작으면 글 버퍼 사이즈만큼,
+			// 크다면 오버한만큼 자릅니다.
+			memcpy( sendbuffer.buffer, buffer.c_str(), length > sendbuffer.length ? sendbuffer.length : length );
+		}
+		proxySocket.C_Send( &sendbuffer );
+
+		broadCastSocket.C_Recv();
+
+		buffer = "";
+	}
+
 	WSACleanup();
 }
---
+++ TestHeadingClientProject/psudoPCH.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+#include <stdint.h>
+#include <time.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <ws2ipdef.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#include <Windows.h>
+	
+#include <memory>
+#include <string>
+#include <stdexcept>
+
+#include <thread>
+#include <vector>
+#include <unordered_map>
+#include <string>
+#include <queue>
+
+#include "define.h"
+#include "Util.h"
+
+#include "CPacketParser.h"
+
+#include "SimpleLock_Win.h"
+
+#include "PrintLog.h"
+
+#include "CMessage_BroadCast.h"
+
+class CSession;
+#include "CSocket.h"
+#include "TestSock.h"
+
+#include "CSession.h"
